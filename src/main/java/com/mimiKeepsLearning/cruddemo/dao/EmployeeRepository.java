e5a023f5b1c1e2cbdf368293c63503c4c4f33cd6
Mariem Moula <moula.meriame@gmail.com>
2024-05-21 11:38:07 +0100

    Initial commit

---
+++ src/main/java/com/mimiKeepsLearning/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.mimiKeepsLearning.cruddemo.dao;
+
+import com.mimiKeepsLearning.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path ="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    // that s it ... no need to write any other code
+}
---
+++ src/test/java/com/mimiKeepsLearning/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimiKeepsLearning.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
